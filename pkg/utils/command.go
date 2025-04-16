3335a31a2888e9b385dece64cc93f7333c04b056
Archisman <archisman@obmondo.com>
2025-04-16 07:34:54 +0530

    feat : sensoring out credentials from logs
    Signed-off-by: Archisman <archisman@obmondo.com>

--- pkg/utils/command.go
+++ pkg/utils/command.go
@@ -4,21 +4,30 @@ import (
 	"context"
 	"log/slog"
 	"os/exec"
+	"strings"
 
+	"github.com/Obmondo/kubeaid-bootstrap-script/pkg/config"
+	"github.com/Obmondo/kubeaid-bootstrap-script/pkg/constants"
+	"github.com/Obmondo/kubeaid-bootstrap-script/pkg/globals"
 	"github.com/Obmondo/kubeaid-bootstrap-script/pkg/utils/assert"
 )
 
 func executeCommand(command string, panicOnExecutionFailure bool) (string, error) {
-	slog.Info("Executing command", slog.String("command", command))
+	slog.Info("Executing command", slog.String("command", sensorCredentials(command)))
 
 	output, err := exec.Command("bash", "-c", command).CombinedOutput()
+	sensoredOutput := sensorCredentials(string(output))
+
 	if panicOnExecutionFailure {
-		assert.AssertErrNil(context.Background(), err, "Command execution failed", slog.String("output", string(output)))
+		assert.AssertErrNil(context.Background(), err,
+			"Command execution failed",
+			slog.String("output", sensoredOutput),
+		)
 	}
 
 	// Print out command execution output (if any).
 	if len(output) > 0 {
-		slog.Debug("Command executed", slog.String("output", string(output)))
+		slog.Debug("Command executed", slog.String("output", sensoredOutput))
 	}
 
 	return string(output), err
@@ -33,4 +42,28 @@ func ExecuteCommand(command string) (string, error) {
 func ExecuteCommandOrDie(command string) string {
 	output, _ := executeCommand(command, true)
 	return output
+}
+
+// Before logging out the command / command execution result, we want to sensor out any contained
+// credentials.
+func sensorCredentials(input string) string {
+	toBeSensoredList := []string{}
+
+	// Determine credentials which need to be sensored.
+	switch globals.CloudProviderName {
+	case constants.CloudProviderAzure:
+		azureSecretsConfig := config.ParsedSecretsConfig.Azure
+
+		toBeSensoredList = append(toBeSensoredList,
+			azureSecretsConfig.ClientID,
+			azureSecretsConfig.ClientSecret,
+		)
+	}
+
+	// Sensor each of those credentials.
+	for _, toBeSensored := range toBeSensoredList {
+		input = strings.ReplaceAll(input, toBeSensored, "**********")
+	}
+
+	return input
 }
