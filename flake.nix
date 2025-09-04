4905630e71f89fa5d623fb3407c98c48e69ac505
Archisman <archisman@obmondo.com>
2025-09-04 07:44:32 +0530

    chore(nix): using devenv to pull cached binaries from Cachix
    Signed-off-by: Archisman <archisman@obmondo.com>

--- flake.nix
+++ flake.nix
@@ -2,8 +2,15 @@
   description = "KubeAid CLI development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     flake-utils.url = "github:numtide/flake-utils";
+
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs =
@@ -11,39 +18,72 @@
       self,
       nixpkgs,
       flake-utils,
-    }:
+      devenv,
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs {
           inherit system;
+
           config.allowUnfree = true;
         };
       in
       with pkgs;
       {
-        devShells.default = mkShell {
-          nativeBuildInputs = [
-            go
-            golangci-lint
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
 
-            bun
-            addlicense
-            pre-commit
-            nix-update
-          ];
+          modules = [
+            (
+              { pkgs, config, ... }:
+              {
+                # Most packages come pre-built with binaries provided by the official Nix binary
+                # cache. If you're modifying a package or using a package that's not built
+                # upstream, Nix will build it from source instead of downloading a binary.
+                # To prevent packages from being built more than once, devenv provides seamless
+                # integration with binary caches hosted by Cachix.
+                #
+                # Devenv will automatically configure Cachix caches for you, or guide you how to
+                # add the caches to Nix manually. Any caches set up by devenv are used in addition
+                # to the caches configured in Nix, for example, in /etc/nix/nix.conf.
+                cachix = {
+                  enable = true;
 
-          buildInputs = [
-            # Required for building KubePrometheus.
-            gojsontoyaml
-            jsonnet
-            jsonnet-bundler
-            jq
+                  # devenv.cachix.org is added to the list of pull caches by default. It mirrors
+                  # the official NixOS cache and is designed to provide caching for the
+                  # devenv-nixpkgs/rolling nixpkgs input.
+                  #
+                  # Some languages and integrations may automatically add caches when enabled.
+                  pull = [ ];
+                };
 
-            kubectl
-            kubeone
-            clusterctl
-            cilium-cli
+                packages =
+                  with pkgs;
+                  [
+                    go
+                    golangci-lint
+
+                    bun
+                    addlicense
+                    pre-commit
+                    nix-update
+                  ]
+                  # Runtime dependencies.
+                  ++ [
+                    # Required for building KubePrometheus.
+                    gojsontoyaml
+                    jsonnet
+                    jsonnet-bundler
+                    jq
+
+                    kubectl
+                    kubeone
+                    clusterctl
+                    cilium-cli
+                  ];
+              }
+            )
           ];
         };
 
